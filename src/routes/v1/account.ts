ef683f4b68acea51fe27dcc415bd39fbd2eceb3c
Evi Owhotemu <evicoach@gmail.com>
2022-08-17 19:09:04 +0100

    Add account management routes

--- src/routes/v1/account.ts
+++ src/routes/v1/account.ts
@@ -1,8 +1,14 @@
-import { Router } from 'express';
+import { Router } from "express";
+import accountController from "../../app/account/account_controller";
+import accountValidator from "../../app/account/account_validator";
 
 const router = Router();
 
-router.post("/signup"); // fund account
-router.get("/login"); // widthraw
+router.post(
+  "/signup",
+  accountValidator.createCustomer,
+  accountController.signup
+); // fund account
+router.post("/login", accountValidator.login, accountController.login); // widthraw
 
 export default router;
