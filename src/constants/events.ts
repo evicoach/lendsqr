7a66b41ff295391ebbc064d618537d685df9942f
Evi Owhotemu <evicoach@gmail.com>
2022-08-17 19:04:22 +0100

    Add constant files

---
+++ src/constants/events.ts
@@ -0,0 +1,10 @@
+export const EVENTS =  {
+        CUSTOMER: {
+            CREATED: "customer_created",
+            DELETED: "customer_deleted",
+            UPDATED: "customer_updated",
+    },
+    TRANSACTION: {
+        COMPLETED: "transaction_completed"
+    }
+}
